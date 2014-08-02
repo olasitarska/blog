5f41ca80f03f5d42b26bc444b4464f34125faa5a
Ola Sitarska <ola@sitarska.com>
2014-08-02 18:43:04 +0200

    Added namespace to urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'', include('posts.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    (r'', include('posts.urls', namespace='posts', app_name='posts')),
+    (r'^admin/', include(admin.site.urls)),
 )
