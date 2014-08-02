9f272e7aea634aa7236aa2e769c285d81a38221a
Ola Sitarska <ola@sitarska.com>
2014-08-02 18:51:19 +0200

    Displaying real posts

--- posts/models.py
+++ posts/models.py
@@ -3,12 +3,22 @@ import datetime
 from django.contrib.auth.models import User
 from django.db import models
 
+class PostManager(models.Manager):
+
+    def get_query_set(self):
+
+        return super(PostManager, self).get_query_set().filter(published_date__lte=datetime.datetime.now())
+
+
 class Post(models.Model):
     author = models.ForeignKey(User)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_date = models.DateTimeField(default=datetime.datetime.now())
     published_date = models.DateTimeField(blank=True, null=True)
+
+    objects = models.Manager()
+    published = PostManager()
 
     def __unicode__(self):
         return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import Post
 
 def post_list(request):
-    return render(request, 'posts/post_list.html', {})
+
+    if request.user.is_authenticated():
+        post_list = Post.objects.all().order_by('-created_date')
+    else:
+        post_list = Post.published.all().order_by('-published_date')
+
+    return render(request, 'posts/post_list.html', {
+        'post_list': post_list
+    })
