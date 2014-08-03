59e73dbbe541223790618959a6b31a1c6929de6e
Ola Sitarska <ola@sitarska.com>
2014-08-03 10:32:44 +0200

    Missing import

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
     if request.method == 'POST':
